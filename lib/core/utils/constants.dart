a05d2fd79c46a9096128f46a428a8f45d38cdf89
weisro-source <nassimsolimanflutter@gmail.com>
2025-04-26 03:50:03 +0300

    add background image

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,9 +1,10 @@
 class ImagePath {
   ImagePath._();
-  
+
   static const String _basePath = "assets/images";
   //! -------- Splash Images ------------
   static const String splashImage = "$_basePath/splash/splashImage.svg";
+  static const String newSplashImage = "$_basePath/splash/splach_image.png";
 
   //! ------- OnBoarding Images -----------
   static const String onBoardingPath = "$_basePath/onBoarding";
